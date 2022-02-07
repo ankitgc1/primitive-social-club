3ff3319387d6a11a59aa243110a45b66f86421b2
ankit <ankitgc1@gmail.com>
2022-02-07 13:02:37 +0530

    Updates

--- precache-manifest.0fef0f4e2270ecfdbf9fbf37797be09d.js
+++ precache-manifest.0fef0f4e2270ecfdbf9fbf37797be09d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7e984dde2e28864dd57098d25e18b438",
+    "revision": "1d2fc6ac6cd6bb3dfcc9732d6f2ad332",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.68a81a89.chunk.css"
   },
   {
-    "revision": "3c103255976c75b241e3",
+    "revision": "fea5c03409efb287f186",
     "url": "/static/css/main.8737b98f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.0314bd12.chunk.js"
   },
   {
-    "revision": "3c103255976c75b241e3",
-    "url": "/static/js/main.cb2527d2.chunk.js"
+    "revision": "fea5c03409efb287f186",
+    "url": "/static/js/main.ad984f0a.chunk.js"
   },
   {
     "revision": "c1928dc21d7e3e4d90d0",
@@ -30,6 +30,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f92b924ed0bbf7d6d61c4aa9e547d848",
     "url": "/static/media/bg.f92b924e.png"
+  },
+  {
+    "revision": "5ca9493d39a76e5c5e5b1c1099551da7",
+    "url": "/static/media/img1.5ca9493d.png"
+  },
+  {
+    "revision": "5ca9493d39a76e5c5e5b1c1099551da7",
+    "url": "/static/media/img2.5ca9493d.png"
+  },
+  {
+    "revision": "5ca9493d39a76e5c5e5b1c1099551da7",
+    "url": "/static/media/img3.5ca9493d.png"
+  },
+  {
+    "revision": "5ca9493d39a76e5c5e5b1c1099551da7",
+    "url": "/static/media/img4.5ca9493d.png"
   },
   {
     "revision": "f8db3e628e28bdddafe49c2083c71fb5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00b5989e48ab0a940824919d23dcd9ca.js"
+  "/precache-manifest.0fef0f4e2270ecfdbf9fbf37797be09d.js"
 );
 
 self.addEventListener('message', (event) => {
